c9f9c118b292c1f404770c9e0c49b28d4107eed6
brushtool <brushtool@tutanota.com>
2020-04-30 00:06:28 +0100

    Steamroll choice prompt

--- gen_deb.sh
+++ gen_deb.sh
@@ -10,16 +10,6 @@ dew_it()
 
 echo "Warning: this resets permissions in the packages folder to the current user."
 
-while true
-do
-    read -r -p 'Do you want to continue? ' choice
-    case "$choice" in
-      n|N) exit 1;;
-      y|Y) break;;
-      *) echo 'Response not valid';;
-    esac
-done
-
 if [ -z $1 ]
     then
         choice="all"
