0a5db8f5363434b720ef084fc29c0b625d3a7946
CrashOveride95 <crashoveride953@gmail.com>
2020-04-30 09:55:50 -0400

    Added n64-demos

--- gen_deb.sh
+++ gen_deb.sh
@@ -23,12 +23,13 @@ mkdir -p debs
 sudo chown -R root:root ./packages
 
 case $choice in
-   "all") dew_it spicy makemask iquesdk n64sdk u64assets root-compatibility-enviroment rsp-tools vadpcm-tools n64graphics libkmc libhvqm libnusys n64manual n64chain-gcc;;
+   "all") dew_it spicy makemask iquesdk n64sdk u64assets n64-demos root-compatibility-enviroment rsp-tools vadpcm-tools n64graphics libkmc libhvqm libnusys n64manual n64chain-gcc;;
    "spicy") dew_it spicy;;
    "makemask") dew_it makemask;;
    "ique") dew_it iquesdk;;
    "n64") dew_it n64sdk;;
    "assets") dew_it u64assets;;
+   "demos") dew_it n64-demos;;
    "root") dew_it root-compatibility-enviroment;;
    "rsp") dew_it rsp-tools;;
    "pcm") dew_it vadpcm-tools;;
