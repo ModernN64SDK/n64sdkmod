875b5e91e4273a5785c51babb41c1db741b2d494
CrashOveride95 <crashoveride953@gmail.com>
2021-11-22 14:16:55 -0500

    Add libleo

--- gen_deb.sh
+++ gen_deb.sh
@@ -23,11 +23,12 @@ mkdir -p debs
 sudo chown -R root:root ./packages
 
 case $choice in
-   "all") dew_it spicy makemask iquesdk n64sdk u64assets root-compatibility-environment rsp-tools vadpcm-tools n64-conv-tools n64-midi-tools n64graphics libhvq libhvqm libnusys libnustd libnaudio libmus n64manual n64-demos nusys-demos kantan-demos mus-demos tutorial-demos n64sdk-common;;
+   "all") dew_it spicy makemask iquesdk n64sdk u64assets root-compatibility-environment rsp-tools vadpcm-tools n64-conv-tools n64-midi-tools n64graphics libhvq libhvqm libnusys libnustd libnaudio libmus libleo n64manual n64-demos nusys-demos kantan-demos mus-demos tutorial-demos n64sdk-common;;
    "spicy") dew_it spicy;;
    "makemask") dew_it makemask;;
    "ique") dew_it iquesdk;;
    "n64") dew_it n64sdk;;
+   "leo") dew_it libleo;;
    "assets") dew_it u64assets;;
    "demos") dew_it n64-demos;;
    "root") dew_it root-compatibility-environment;;
